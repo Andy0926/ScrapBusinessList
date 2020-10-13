9fa44fcdf8ebda7db892ec9b5cd92b12a1384590
Andy0926 <tanandy2011@hotmail.com>
2020-10-13 18:05:11 +0800

    Refractor Code

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const { default: Axios } = require("axios");
 const cheerio = require("cheerio");
-const {writeFile} = require('fs');
+const { writeFile } = require('fs');
 
-// const ax = require('axios').create({
-//     baseURL: 'https://www.businesslist.my/location/kuala-lumpur'
-// });
-//const colors = require("colors");
-
-const getData = async (pageNum, linkLength) => {
-    //await ax.get('/company/377236/klinik-dr-lo').then(rsp => {console.log(rsp.data)})
+const getData = async (pageNum) => {
     var mainLink = `https://www.businesslist.my`;
 
     const axios = require("axios").create({
@@ -16,12 +10,11 @@ const getData = async (pageNum, linkLength) => {
         headers: { 'X-Custom-Header': 'foobar' }
     });
 
-    const html = await axios.get('/location/kuala-lumpur').then(rsp => rsp.data);
+    const html = await axios.get(`/location/kuala-lumpur/${pageNum}`).then(rsp => rsp.data);
     const $ = cheerio.load(html);
     const arrayList = [];
 
     $('.company').each((a, el) => {
-        //console.log($(el).find('h4 a').attr("href"));
         var subLink = $(el).find('h4 a').attr("href");
 
         subLink ? arrayList.push(subLink) : null;
@@ -29,30 +22,25 @@ const getData = async (pageNum, linkLength) => {
 
     //Go to SubLink Page
     //Cannot do like this? company name is empty
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < arrayList.length; i++) {
         const subHtml = await axios.get(arrayList[i])
             .then(rsp => rsp.data).catch(err => console.log(err));
 
-
         const $ = cheerio.load(subHtml);
         const companyName = $('#company_name').text().trim();
-        console.log(companyName);
-        // console.log('company name: ' + companyName);
-        // break;
+        console.log('company name: ' + companyName);
+    }
+    if (pageNum < 3) {
+        console.log();
+        pageNum+=1;
+        console.log("========");
+        console.log("Page "+pageNum);
+        console.log("========");
+        return getData(pageNum);
     }
 
 }
 
 (async () => {
-    await getData(1, 51);
-
-    // const html = await axios.get('https://www.businesslist.my/company/380133/money-life-research').then(rsp => {
-    //     return rsp.data
-    // });
-
-    // const $ = cheerio.load(html);
-    // const companyName = $('#company_name').text().trim();
-
-    // console.log(companyName);
-
+    await getData(1);
 })();
