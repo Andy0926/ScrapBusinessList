0c2d80d0ea1045d53a8d594db67665c0db025c50
Andy0926 <tanandy2011@hotmail.com>
2020-10-13 17:19:21 +0800

    2nd commit

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const cheerio = require("cheerio");
-const axios = require("axios");
+const axios = require("axios").create({
+    baseURL: "https://www.businesslist.my/location/kuala-lumpur/1",
+    //address: "https://www.businesslist.my/location/kuala-lumpur/1",
+    headers: {'X-Custom-Header': 'foobar'}
+});
+
+const { url } = require("inspector");
 //const colors = require("colors");
 
 const getData = async (pageNum, linkLength) => {
 
     var mainLink = `https://www.businesslist.my/location/kuala-lumpur/${pageNum}`;
 
-    const html = await axios.get(mainLink)
+    const html = await axios.get()
         .then(rsp => rsp.data);
     const $ = cheerio.load(html);
     const arrayList = [];
@@ -20,10 +26,10 @@ const getData = async (pageNum, linkLength) => {
             subLink = '';
         }
         else {
-            var fullSubLink = mainLink + subLink;
-            console.log(fullSubLink);
-            arrayList.push(fullSubLink);
-            console.log(arrayList.length);
+            //var fullSubLink = `https://www.businesslist.my/location/kuala-lumpur/${pageNum}` + subLink;
+            //console.log(fullSubLink);
+            arrayList.push(subLink);
+            //console.log(arrayList.length);
             //console.log('length is = ' + fullSubLink.length)
             console.log();
 
@@ -31,16 +37,25 @@ const getData = async (pageNum, linkLength) => {
 
     });
 //Cannot do like this? company name is empty
+
     for (var i = 0; i < arrayList.length; i++) {
         
-        //console.log(arrayList[i]);
+        console.log(arrayList[i]);
+
+
+        // const subHtml = await axios.get(instance.defaults.baseURL+arrayList[i]).then(rsp=>{
+        //     return rsp.data
+        // });
+
         const subHtml = await axios.get(arrayList[i]).then(rsp=>{
             return rsp.data
-        });
+        }).catch(err => console.log(err));
+
         const $ = cheerio.load(subHtml);
         const companyName = $('#company_name').text().trim();
 
         console.log('company name: '+ companyName);
+        break;
     }
 
 }
@@ -48,13 +63,13 @@ const getData = async (pageNum, linkLength) => {
 (async () => {
     await getData(1, 51);
 
-    // const html = await axios.get('https://www.businesslist.my/company/380133/money-life-research').then(rsp => {
-    //     return rsp.data
-    // });
+    const html = await axios.get('https://www.businesslist.my/company/380133/money-life-research').then(rsp => {
+        return rsp.data
+    });
 
-    // const $ = cheerio.load(html);
-    // const companyName = $('#company_name').text().trim();
+    const $ = cheerio.load(html);
+    const companyName = $('#company_name').text().trim();
 
-    // console.log(companyName);
+    console.log(companyName);
 
 })();
