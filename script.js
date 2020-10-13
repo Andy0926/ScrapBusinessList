48b4c79982f695a678e5f02c877355b9e3fd4373
Andy0926 <tanandy2011@hotmail.com>
2020-10-13 17:50:31 +0800

    ScrapSublink

--- script.js
+++ script.js
@@ -1,61 +1,44 @@
+const { default: Axios } = require("axios");
 const cheerio = require("cheerio");
-const axios = require("axios").create({
-    baseURL: "https://www.businesslist.my/location/kuala-lumpur/1",
-    //address: "https://www.businesslist.my/location/kuala-lumpur/1",
-    headers: {'X-Custom-Header': 'foobar'}
-});
+const {writeFile} = require('fs');
 
-const { url } = require("inspector");
+// const ax = require('axios').create({
+//     baseURL: 'https://www.businesslist.my/location/kuala-lumpur'
+// });
 //const colors = require("colors");
 
 const getData = async (pageNum, linkLength) => {
+    //await ax.get('/company/377236/klinik-dr-lo').then(rsp => {console.log(rsp.data)})
+    var mainLink = `https://www.businesslist.my`;
 
-    var mainLink = `https://www.businesslist.my/location/kuala-lumpur/${pageNum}`;
+    const axios = require("axios").create({
+        baseURL: mainLink,
+        headers: { 'X-Custom-Header': 'foobar' }
+    });
 
-    const html = await axios.get()
-        .then(rsp => rsp.data);
+    const html = await axios.get('/location/kuala-lumpur').then(rsp => rsp.data);
     const $ = cheerio.load(html);
     const arrayList = [];
-    const title = $('.company').each((a, el) => {
+
+    $('.company').each((a, el) => {
         //console.log($(el).find('h4 a').attr("href"));
         var subLink = $(el).find('h4 a').attr("href");
-        console.log('sublink is = ' + subLink);
 
-        if (typeof subLink == 'undefined') {
-            console.log("@@@@@@@@@@@@@");
-            subLink = '';
-        }
-        else {
-            //var fullSubLink = `https://www.businesslist.my/location/kuala-lumpur/${pageNum}` + subLink;
-            //console.log(fullSubLink);
-            arrayList.push(subLink);
-            //console.log(arrayList.length);
-            //console.log('length is = ' + fullSubLink.length)
-            console.log();
-
-        }
-
+        subLink ? arrayList.push(subLink) : null;
     });
-//Cannot do like this? company name is empty
 
-    for (var i = 0; i < arrayList.length; i++) {
-        
-        console.log(arrayList[i]);
+    //Go to SubLink Page
+    //Cannot do like this? company name is empty
+    for (var i = 0; i < 1; i++) {
+        const subHtml = await axios.get(arrayList[i])
+            .then(rsp => rsp.data).catch(err => console.log(err));
 
-
-        // const subHtml = await axios.get(instance.defaults.baseURL+arrayList[i]).then(rsp=>{
-        //     return rsp.data
-        // });
-
-        const subHtml = await axios.get(arrayList[i]).then(rsp=>{
-            return rsp.data
-        }).catch(err => console.log(err));
 
         const $ = cheerio.load(subHtml);
         const companyName = $('#company_name').text().trim();
-
-        console.log('company name: '+ companyName);
-        break;
+        console.log(companyName);
+        // console.log('company name: ' + companyName);
+        // break;
     }
 
 }
@@ -63,13 +46,13 @@ const getData = async (pageNum, linkLength) => {
 (async () => {
     await getData(1, 51);
 
-    const html = await axios.get('https://www.businesslist.my/company/380133/money-life-research').then(rsp => {
-        return rsp.data
-    });
+    // const html = await axios.get('https://www.businesslist.my/company/380133/money-life-research').then(rsp => {
+    //     return rsp.data
+    // });
 
-    const $ = cheerio.load(html);
-    const companyName = $('#company_name').text().trim();
+    // const $ = cheerio.load(html);
+    // const companyName = $('#company_name').text().trim();
 
-    console.log(companyName);
+    // console.log(companyName);
 
 })();
