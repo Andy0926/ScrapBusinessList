f1e4d9ccc68b9a25a454acd2e523f33065173317
Andy0926 <tanandy2011@hotmail.com>
2020-10-14 11:36:39 +0800

    update

--- script.js
+++ script.js
@@ -24,32 +24,68 @@ const getData = async (pageNum) => {
     //Go to SubLink Page
     //Cannot do like this? company name is empty
     for (var i = 0; i < arrayList.length; i++) {
+
+        var obj = {
+            tableData: []
+        };
+
         const subHtml = await axios.get(arrayList[i])
             .then(rsp => rsp.data).catch(err => console.log(err));
 
         const $ = cheerio.load(subHtml);
-        const companyName = $('#company_name').text().trim();
-        const address = $('.location').text().trim();
-        const phoneNumber = $('.phone').text().trim();
-        const webLink = $('.weblinks a').attr("href").trim();
-        const testText =$('.cmp_details_in').not('div:nth-child(2)').text().trim();
+        try {
+            const companyName = $('#company_name').text().trim();
+            const address = $('.location').text().trim();
+            const phoneNumber = $('.phone').text().trim();
+            const webLink = $('.weblinks a').attr("href").trim();
+            const detail = $('.cmp_details_in').not('div:nth-child(2)').text().trim();
+
+            console.log('Company Name: ' + companyName.cyan,
+                '\nAddress: ' + address + '\nPhone: ' + phoneNumber + '\nWebsite: ' + webLink);
+            console.log(detail);
+
+            var objData = {
+                strCompanyName: companyName,
+                strPhoneNumber: phoneNumber,
+                strAddress: address,
+                strWebLink: webLink,
+                strDetail: detail
+            };
+
+
+            fs.readFile("data.json", function readFileCallBack(err, data) {
+                if (err) throw err
+                else {
+                    obj = JSON.parse(data);
+                    obj.tableData.push(objData);
+                    json = JSON.stringify(obj);
+                    //console.log(json);
+                    fs.writeFile('data.json', json, function (err) {
+                        if (err) throw err;
+                    });
+                    console.log("datainputsuccess")
+                };
+            });
+
+        } catch (err) {
+            console.log(err)
+        }
+
         //const testText =$('.cmp_details_in div:nth-child(2)').text().trim();
-       
+
         //print out single vat registration line
         //const testText = $('.cmp_details_in div:nth-child(15)').text().trim();
-        
+
         //div:nth-child(2)
 
-        console.log('Company Name: ' + companyName.cyan,
-            '\nAddress: ' + address + '\nPhone: ' + phoneNumber + '\nWebsite: ' + webLink);
-        console.log(testText);
+
 
     }
-    if (pageNum < 3) {
+    if (pageNum < 100) {
         console.log();
-        pageNum+=1;
+        pageNum += 1;
         console.log("========");
-        console.log("Page "+pageNum);
+        console.log("Page " + pageNum);
         console.log("========");
         //break;
         return getData(pageNum);
